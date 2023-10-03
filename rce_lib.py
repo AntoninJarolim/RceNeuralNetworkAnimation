972c86d8fa5c9b80bf08a62c4a52276c24670144
Antonín Jarolím <jarolim.antonin@seznam.cz>
2023-10-03 19:43:41 +0200

    Done

--- rce_lib.py
+++ rce_lib.py
@@ -26,27 +26,41 @@ class RceNeuralNetwork:
         # events initialization
         self.create_neuron_event = Events()
         self.shrink_neuron_radius = Events()
+        self.correct_point_appears = Events()
+        self.blink_incorrect_neuron = Events()
 
         # alg initialization
         self.neurons = []
 
     def start(self):
-        for coordinates, expected in zip(self.points, self.expected_values):
-            if len(self.neurons) == 0:
-                self.create_neuron(coordinates, expected)
-                continue
-
-            for neuron in self.neurons:
-                if neuron.contains(coordinates):
-                    if neuron.representing_class == expected:
-                        # Neuron is correctly classing examined value
-                        pass
-                    else:
-                        # Neuron does not correctly classify, shrinking needed
-                        self.shrink_neuron_radius.on_change(neuron, expected)
-                else:
-                    # Point is not inside any neuron, new neuron needed
+        modification = True
+        while modification:
+            modification = False
+            for coordinates, expected in zip(self.points, self.expected_values):
+                x, y = coordinates[0], coordinates[1]
+                if len(self.neurons) == 0:
                     self.create_neuron(coordinates, expected)
+                    continue
+
+                point_belongs_somewhere = False
+                for neuron in self.neurons:
+                    if neuron.contains(coordinates):
+                        point_belongs_somewhere = True
+                        if neuron.representing_class == expected:
+                            # Neuron is correctly classing examined value
+                            self.correct_point_appears.on_change(x, y, expected)
+                        else:
+                            # Neuron does not correctly classify, shrinking needed
+                            self.blink_incorrect_neuron.on_change(x, y, expected)
+                            neuron.radius = neuron.radius / 2
+                            self.shrink_neuron_radius.on_change(neuron)
+                            modification = True
+
+                # Point is not inside any neuron, new neuron needed
+                if point_belongs_somewhere is False:
+                    self.create_neuron(coordinates, expected)
+                    modification = True
+
     def create_neuron(self, coordinates, expected):
         neuron = RceNeuron(coordinates, expected)
         self.neurons.append(neuron)
--- scene.py
+++ scene.py
@@ -5,11 +5,12 @@ import pandas as pd
 
 
 def get_data():
-    with open('data.json') as f:
+    with open('data2.json') as f:
         return json.load(f)
 
+
 def get_neuron_color(expected):
-    colors = [BLACK, RED, BLUE]
+    colors = [BLACK, RED, BLUE, GREEN]
     return colors[expected]
 
 
@@ -18,8 +19,8 @@ class RceNetworkCreation(Scene):
         super().__init__()
         self.axes = Axes(x_range=[-3, 8], y_range=[-2, 4], x_length=11, y_length=6).add_coordinates()
         self.origin = self.axes.get_origin()
-        self.neuron_radiuses = pd.DataFrame(columns=['x', 'y', 'radius'])
-
+        self.neuron_radiuses = pd.DataFrame(columns=['x', 'y', 'neuron'])
+        self.points = []
         # circ = Circle(radius=0.5).move_to([-4, -1.5, 0])
 
     def construct(self):
@@ -36,8 +37,12 @@ class RceNetworkCreation(Scene):
         rce_problem = rce.RceNeuralNetwork(rce_data)
         rce_problem.create_neuron_event.on_change += self.add_neuron
         rce_problem.shrink_neuron_radius.on_change += self.shrink_neuron_radius
+        rce_problem.correct_point_appears.on_change += self.correct_point_appears
+        rce_problem.blink_incorrect_neuron.on_change += self.blink_incorrect_neuron
+
         rce_problem.start()
-        self.wait()
+
+        self.wait(1)
 
     def add_points(self, points):
         dots = [Dot(coordinates) for coordinates in points]
@@ -49,22 +54,112 @@ class RceNetworkCreation(Scene):
         # Get poit according to axes coordination
         point = self.axes.coords_to_point(neuron.x, neuron.y)
 
+        # Add radius around a neuron
+        neuron_dot = Dot(point=point, radius=neuron.radius, stroke_width=1.3, color=neuron_color,
+                         fill_color=neuron_color, fill_opacity=0.08)
+        neuron_record = {'x': neuron.x, 'y': neuron.y, 'neuron': neuron_dot}
+        self.neuron_radiuses.loc[len(self.neuron_radiuses)] = neuron_record
+        self.add(neuron_dot)  # show the circle on screen
+
         # Add point representing middle of a neuron
         dot_point = Dot(point=point, radius=0.12, color=neuron_color)
         self.add(dot_point)
+        self.points.append(str(neuron.x) + str(neuron.y))
+        self.wait(0.5)
 
-        # Add radius around a neuron
-        neuron_radius = Dot(point=point, radius=neuron.radius, stroke_width=1.3, color=neuron_color,
-                            fill_color=neuron_color, fill_opacity=0.08)
-        neuron_record = {'x': neuron.x, 'y': neuron.y, 'radius': neuron_radius}
-        self.neuron_radiuses.loc[len(self.neuron_radiuses)] = neuron_record
-        # neuron_radius.set_fill(neuron_color, opacity=0.08)  # set the color and transparency
-        self.add(neuron_radius)  # show the circle on screen
+    def shrink_neuron_radius(self, neuron):
+        # select correct neuron radius
+        row = self.neuron_radiuses[
+            (self.neuron_radiuses['x'] == neuron.x) &
+            (self.neuron_radiuses['y'] == neuron.y)
+            ]
+        neuron_dot = row.squeeze()
+        neuron_radius = neuron_dot['neuron']
 
-    def shrink_neuron_radius(self, neuron, expected):
+        # Create new neuron using old neuron
+        new_radius = neuron_radius.radius / 2
+        point = self.axes.coords_to_point(neuron.x, neuron.y)
+        new_neuron = Dot(point=point, radius=new_radius, stroke_width=neuron_radius.stroke_width,
+                         color=neuron_radius.color, fill_color=neuron_radius.fill_color,
+                         fill_opacity=neuron_radius.fill_opacity)
 
-        pass
+        # Play transformation before removing neuron
+        self.play(
+            Transform(neuron_radius, new_neuron, replace_mobject_with_target_in_scene=True)
+        )
+
+        # Replace old neuron (drop one row, create new row, add new row to df)
+        self.neuron_radiuses = self.neuron_radiuses.drop(row.index)
+        neuron_record = {'x': neuron.x, 'y': neuron.y, 'neuron': new_neuron}
+        self.neuron_radiuses = self.neuron_radiuses._append(neuron_record, ignore_index=True)
+
+        self.wait(0.5)
+
+    def correct_point_appears(self, x, y, expected):
+        str_repr = str(x) + str(y)
+        for point_str_repr in self.points:
+            if point_str_repr == str_repr:
+                return
+        self.points.append(str_repr)
+
+        neuron_color = get_neuron_color(expected)
+        # Get poit according to axes coordination
+        point = self.axes.coords_to_point(x, y)
+        # Add point representing middle of a neuron
+        dot_point = Dot(point=point, radius=0.12, color=neuron_color)
+        self.add(dot_point)
+        self.wait(duration=0.5)
+
+    def blink_incorrect_neuron(self, x, y, expected):
+        neuron_color = get_neuron_color(expected)
+        # Get poit according to axes coordination
+        point = self.axes.coords_to_point(x, y)
+        point[2] = 36
+        # Add point representing middle of a neuron
+
+        for _ in range(3):
+            dot_point = Dot(point=point, radius=0.16, color=neuron_color)
+            self.add(dot_point)
+            self.wait(duration=0.15)
+            self.remove(dot_point)
+            self.wait(duration=0.15)
 
 
-animation = RceNetworkCreation()
-animation.construct()
+if __name__ == "__main__":
+    animation = RceNetworkCreation()
+    animation.construct()
+
+    neuron_radiuses = pd.DataFrame(columns=['x', 'y', 'neuron', 'neuron_radius'])
+
+    neuron_dot = Dot(point=LEFT, radius=3)
+    neuron_record = {'x': 0, 'y': 0, 'neuron': neuron_dot, 'neuron_radius': 3}
+
+
+
+    row = neuron_radiuses[
+        (neuron_radiuses['x'] == 0) &
+        (neuron_radiuses['y'] == 0)
+        ]
+    neuron_dot = row.squeeze()
+
+    new_neuron_dot = Dot(point=RIGHT, radius=1.5)
+
+    # Replace old neuron
+    neuron_as_series = pd.Series(new_neuron_dot)
+    neuron_radiuses.loc[
+        (neuron_radiuses['x'] == 0) &
+        (neuron_radiuses['y'] == 0)
+        , 'neuron'] = pd.Series(new_neuron_dot)
+
+                            # {'neuron': new_neuron_dot},
+                            # inplace=True)
+
+    row2 = neuron_radiuses[
+        (neuron_radiuses['x'] == 0) &
+        (neuron_radiuses['y'] == 0)
+        ]
+    neuron_dot = row2.squeeze()
+    new_radius = neuron_dot['neuron'].radius / 2
+    pass
+
+
